bcba1047e295e6cb173eb88e662a53a3d9548757
maks_et <maks.ept1@gmail.com>
2020-12-10 15:54:33 +0500

    new function

--- functions.py
+++ functions.py
@@ -42,14 +42,27 @@ def generateKey():
 
 # Функции помощники для встраивания ЦВЗ
 def __W(n, m, W, K, L):
-    print(W[math.floor(n/L), math.floor(m/L)],
-          K[math.floor(n/L), math.floor(m/L)])
+    # print(W[math.floor(n/L), math.floor(m/L)],
+    #      K[math.floor(n/L), math.floor(m/L)])
     return W[math.floor(n/L), math.floor(m/L)] ^ K[math.floor(n/L), math.floor(m/L)]
 
 
 # Функция встраивания ЦВЗ
 def generateWithDWM(main, dwm, key, q):
-    L = len(main)
+    L = len(dwm)
     N = len(dwm)
-    #mainResult = np.round(main/q).astype(np.uint8)
-    print(__W(0, 0, dwm, key, L))
+    # mainResult = np.round(main/q).astype(np.uint8)
+    print('yes', main[0][0][0])
+    print('no',  main[399][399][0])
+    for n, item in enumerate(main):
+        for m, item in enumerate(item):
+            if __W(n, m, dwm, key, L)[i] > 127:
+                # main[n][m][i] = math.floor( main[n][m][i]/q) + (q/2) + (main[n][m][i] % (q/2))
+                print(int(main[n][m][i])/q)
+                pass
+            else:
+                pass
+                # main[n][m][i] = math.floor(int(main[n][m][i]) / q) + (main[n][m][i] % (q/2))
+            # print('')
+    print('Встраивание завершено')
+    # print(__W(0, 0, dwm, key, L))
